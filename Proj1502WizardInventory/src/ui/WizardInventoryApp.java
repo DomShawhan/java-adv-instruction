8201ea2f6f5b63f8d0fb11f01d4f5baa8dad036f
DomShawhan <dfshawhan@yahoo.com>
2024-03-15 18:56:43 -0400

    Updated Wizard Inventory

--- Proj1502WizardInventory/src/ui/WizardInventoryApp.java
+++ Proj1502WizardInventory/src/ui/WizardInventoryApp.java
@@ -8,7 +8,7 @@ public class WizardInventoryApp {
 
 	public static void main(String[] args) {
 		System.out.println("The Wizard Inventory Game");
-		List<String> commands = new ArrayList<>(Arrays.asList("show", "grab", "edit", "drop", "exit"));
+		List<String> commands = new ArrayList<>(Arrays.asList("show", "grab", "edit", "drop", "exit", "help"));
 		String command = "";
 		
 		List<String> items = initializeItems();
@@ -34,6 +34,9 @@ public class WizardInventoryApp {
 				items = edit(items);
 			} else if(command.equalsIgnoreCase(commands.get(3))){
 				items = drop(items);
+			} else if(command.equalsIgnoreCase(commands.get(5))) {
+				System.out.println();
+				printCommand();
 			}
 		}
 		
@@ -59,6 +62,7 @@ public class WizardInventoryApp {
 		System.out.println("edit - Edit an item");
 		System.out.println("drop - Drop an item");
 		System.out.println("exit - Exit Program");
+		System.out.println("help - Display commands");
 		
 	}
 	
@@ -80,19 +84,24 @@ public class WizardInventoryApp {
 	}
 	
 	public static List<String> edit(List<String> items) {
-		int number = Console.getInt("Number: ", 0, items.size() + 1);
-		String updatedName = Console.getString("Updated Name: ");
-		
-		items.set(number - 1, updatedName);
-		System.out.println("Item number " + number + " was updated.");
+		if(items.size() > 0) {
+			int number = Console.getInt("Number: ", 0, items.size() + 1);
+			String updatedName = Console.getString("Updated Name: ");
+			
+			items.set(number - 1, updatedName);
+			System.out.println("Item number " + number + " was updated.");
+		}
 		
 		return items;
 	}
 	
 	public static List<String> drop(List<String> items) {
-		int number = Console.getInt("Number: ", 0, items.size() + 1);
-		
-		items.remove(number - 1);
+		if(items.size() > 0) {
+			int number = Console.getInt("Number: ", 0, items.size() + 1);
+			
+			items.remove(number - 1);
+		}
+
 		
 		return items;
 	}
